1b084132bf91bd8e6388829e9b45a01c5d381c26
StoegerSeb <SkyCrash13241234>
2020-04-05 21:28:45 +0200

    Added Username Search Patterns

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -9,5 +9,14 @@ if (isset($_POST['signup-submit'])) {
 
   if (empty($username) || empty($email) || empty($password) || empty($password_repeat)) {
     header("Location: ../signup.php?error=emptyFields&uid=".$username."&mail=".$email);
+    exit();
+  }
+  else if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    header("Location: ../signup.php?error=invalidMail&uid=".$username);
+    exit();
+  }
+  else if(!preg_match("/^[a-zA-Z0-9]*$/", $username)) {
+    header("Location: ../signup.php?error=invaliduid&mail=".$email);
+    exit();
   }
 }
